be112c58b341d0959f18e46a3e4879a51cd5d803
adham70d-star <adham70d@gmail.com>
2026-06-20 04:19:36 +0300

    add homepage

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{display: 'flex', minHeight: '100vh', alignItems: 'center', justifyContent: 'center', background: '#000', color: '#fff'}}>
+      <h1 style={{fontSize: '2rem', fontWeight: 'bold'}}>MediaBox Downloader شغال ✅</h1>
+    </main>
+  )
+}
